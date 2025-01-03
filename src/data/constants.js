bcff84fef4d7bcfc5a49aff5db7364381bdb5f30
Suraj Sanap <srslinkedin89@gmail.com>
2025-01-03 16:51:00 +0530

    update

---
+++ src/data/constants.js
@@ -0,0 +1,127 @@
+export const Bio = {
+  name: "Suraj Sanap",
+  roles: [
+    "Data Analyst"
+  ],
+  description:
+    "I am a highly skilled and adaptable professional with a passion for data analysis, machine learning, and AI-driven solutions. With a solid foundation in programming and a commitment to continuous learning, I strive to deliver innovative and impactful results in a collaborative environment.",
+  github: "https://github.com/SurajSanap",
+  linkedin: "https://www.linkedin.com/in/surajsanap01/",
+  email:"surajsanapcontact@gmail.com"
+};
+
+export const skills = [
+  {
+    title: "Programming Languages and Frameworks",
+    skills: [
+      { name: "Python", image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" },
+      { name: "Core Java", image: "https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg" },
+      { name: "HTML", image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png" },
+      { name: "CSS", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/1024px-CSS3_logo.svg.png" },
+      { name: "Flask", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNqtHYopo_XXmaa60pJtj_7yNPnYsWU_KkZmtNyHxkPA&s" }
+    ],
+  },
+  {
+    title: "Data Science Expertise",
+    skills: [
+      { name: "Machine Learning", image: "https://cdn-icons-png.flaticon.com/512/8637/8637099.png" },
+      { name: "Data Analysis", image: "https://w7.pngwing.com/pngs/73/309/png-transparent-yellow-graph-increase-icon-or-logo.png" },
+      { name: "Time Series Analysis", image: "https://cdn-icons-png.flaticon.com/512/2939/2939413.png" },
+      { name: "Natural Language Processing", image: "https://cdn.iconscout.com/icon/premium/png-256-thumb/nlp-8926384-7279481.png" },
+      { name: "Generative AI", image: "https://ih1.redbubble.net/image.4911784195.8379/st,small,507x507-pad,600x600,f8f8f8.jpg" }
+    ],
+  },
+  {
+    title: "Tools and Technologies",
+    skills: [
+      { name: "Git", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png" },
+      { name: "LangChain", image: "https://avatars.githubusercontent.com/u/105049481?s=200&v=4" },
+      { name: "Streamlit", image: "https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png" },
+      { name: "HuggingFace", image: "https://huggingface.co/front/assets/huggingface_logo.svg" },
+      { name: "PowerBI", image: "https://upload.wikimedia.org/wikipedia/commons/c/cf/New_Power_BI_Logo.svg" },
+      { name: "SQL", image: "https://symbols.getvecta.com/stencil_28/61_sql-database-generic.90b41636a8.png" }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz1_6NfSgkPKvKAbRNA3X7WLwPaQYf6auLmmG9ihFImw&s",
+    role: "Data Analyst Intern",
+    company: "360iResearch, Pune, India",
+    date: "Sep 2023 - Feb 2024",
+    desc:"Developed a market research tool using Python, ETL workflows, and time series analysis, automating decision-making and enhancing forecasting accuracy by 84%. Collaborated in a Scrum team within an Agile environment.",
+    skills: [
+      "Python",
+      "ETL workflows",
+      "Time Series Analysis",
+      "Scrum",
+      "SQL"
+    ]
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://bit.ly/4gR3yzj",
+    school: "Savitribai Phule Pune University",
+    date: "Jul 2021 - Jun 2025",
+    grade: "8.13 CGPA",
+    desc: "Pursuing Bachelor of Engineering in Information Technology.",
+    degree: "B.E. in Information Technology",
+  },
+
+  {
+    id: 1,
+    
+    school: "Shri Samarth Vidya Mandir Prashala, Ahmednagar",
+    date: "2021",
+    grade: "Percentage: 84.83%",
+    desc: "Completed 12th (MSBSHSE) with a focus on science.",
+    degree: "Class XII",
+  },
+  {
+    id: 2,
+    
+    school: "Shri Shaneshwar Secondary and Higher School, Sonai, Ahmednagar",
+    date: "2019",
+    grade: "Percentage: 79.40%",
+    desc: "Completed 10th grade with distinction.",
+    degree: "Class X",
+  },
+
+  
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "College.ai",
+    description: "AI-based platform to optimize job applications using Generative AI. Features include ATS, Resume Analyzer, and PDF Chat powered by NLP and LLMs.",
+    image: "https://github.com/SurajSanap/College.ai-main/assets/101057653/f5923134-c4c1-4586-975b-3247675bb475",
+    tags: ["Streamlit", "Langchain", "Google Gemini API", "SQLite", "NLP", "LLMs"],
+    github: "https://github.com/SurajSanap/College.ai-main",
+    webapp: "https://collegeai.streamlit.app/"
+  },
+  {
+    id: 2,
+    title: "CultiVision",
+    description: "Developed a website offering real-time crop recommendations with regression and data modeling based on soil and pest analysis.",
+    image: "https://github.com/user-attachments/assets/328fff0d-4ad5-4dd3-b8c5-c1a44fe2fae4",
+    tags: ["HTML", "CSS", "ML", "DL", "Active Learning", "Bayesian Models"],
+    github: "https://github.com/SurajSanap/Cultivision",
+    webapp: "https://placeholder.app"
+  },
+  {
+    id: 3,
+    title: "My Future Stock",
+    description:
+      "Designed and deployed a machine learning model to forecast global stock prices, addressing the challenge of accurate stock market predictions using regression and data modeling techniques.",
+    image: "https://akm-img-a-in.tosshub.com/businesstoday/images/story/202306/gettyimages-1311007681-2afbfba73b744b88ab3bd388ae6c8b00_2-sixteen_nine.jpg?size=948:533", // Replace with the actual image URL
+    tags: ["Python", "Flask", "Regression", "Machine Learning", "SQL"],
+    github: "https://github.com/SurajSanap/My-Future-Stock",
+    webapp: "https://placeholder.app" // Replace with the actual live link if available
+  },
+];
